803f4b471674dd371aa6060c6ba0090715496f5c
Luchian Nicolae <nicolai.luchian@gmail.com>
2021-06-11 22:29:02 +0300

    another commit

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ const star = function (num) {
   }
 };
 star(10);
+
+//const palindromeNumber = function (num) {
+//  const f = num.length;
+//  for (let i = 0; i < num.length / 2; i++) {
+//    if (num[i] !== num[f - 1 - i]) {
+//      alert("It is not a palindrome");
+//    }
+//  }
+//  const string = prompt("Enter a string or number: ");
+//
+//  const value = validatePalin(num);
+//};
+//palindromeNumber(value);
+
+//debugger;
+
+function validatePalin(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return "It is not a palindrome";
+    }
+  }
+  return "It is a palindrome";
+}
+
+console.log(validatePalin("12345678987654321"));
+
+function validate(str) {
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[str.length - 1 - i]);
+    console.log(str.length / 2);
+  }
+}
+validate("1234321");
